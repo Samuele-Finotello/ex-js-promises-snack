2fd8823c449c4046cfac50e9341ecfc6401c7a53
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-11-12 18:11:09 +0100

    [add] feat end snack 1 bonus and fix snack 1

--- Snack 1/snack1.js
+++ Snack 1/snack1.js
@@ -1,9 +1,30 @@
 function getPostTitle(id) {
   return new Promise((resolve, reject) => {
     fetch(`https://dummyjson.com/posts/${id}`)
-      .then(response => response.json())
-      .then(data => console.log(`Il titolo del post n.${id} e': '${data.title}'`))
+      .then(res => res.json())
+      .then(data => resolve(data))
+      .catch(error => reject(error))
   })
 }
 
-getPostTitle(6)
+getPostTitle(5)
+  .then(post => console.log(`Il titolo del post n.${post.id} e': '${post.title}'`))
+  .catch(error => console.log(error))
+
+function getPost(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then(res => res.json())
+      .then(post => {
+        fetch(`https://dummyjson.com/users/${post.userId}`)
+          .then(res => res.json())
+          .then(user => resolve({ ...post, user }))
+          .catch(error => reject(error))
+      })
+      .catch(error => reject(error))
+  })
+}
+
+getPost(13)
+  .then(fullPost => console.log(fullPost))
+  .catch(error => console.error(error))
