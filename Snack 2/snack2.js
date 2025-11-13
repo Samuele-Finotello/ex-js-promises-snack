3c49b3ee80340617f5572cf36fbbc5243b2bc947
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-11-13 10:07:32 +0100

    [add] feat end snack 2 bonus

--- Snack 2/snack2.js
+++ Snack 2/snack2.js
@@ -17,3 +17,39 @@ function lanciaDado() {
 lanciaDado()
   .then(num => console.log(num))
   .catch(messaggio => console.error(messaggio))
+
+function creaLanciaDado() {
+  let ultimoLancio = null;
+
+  return function () {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const stuck = Math.floor(Math.random() * 5)
+
+        if (stuck !== 0) {
+          const num = Math.floor(Math.random() * 6) + 1;
+          if (num === ultimoLancio) {
+            console.log('Incredibile!')
+          }
+          ultimoLancio = num;
+          resolve(`Il numero uscito e': ${num}`)
+        }
+        else {
+          ultimoLancio = null;
+          reject("Il dado si e' incastrato. Riprova.")
+        }
+      }, 3000)
+    })
+  }
+}
+
+const lancioDadi = creaLanciaDado()
+
+lancioDadi()
+  .then(num => {
+    console.log(num)
+    lancioDadi()
+      .then(num => console.log(num))
+      .catch(messaggio => console.error(messaggio))
+  })
+  .catch(messaggio => console.error(messaggio))
